a0557f0260cd40765cf2eb0d2553c34b1ac9b8ad
Mahesh <maheswaram.mahesh@gaiansolutions.com>
2018-10-12 14:19:52 +0530

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+
 import ConsoleCurrentState from './container/consoleCurrentState'
 import FormTask from './container/FormTask';
 
 let App = () => (
-  <div >
-    <ConsoleCurrentState />
-    <hr/>
-    <FormTask />
+  <Router>
+    <div >
+    <nav className="App-nav">
+          <ul>
+            <li><Link to="/task1">Task 1</Link></li>
+            <li><Link to="/debug">Console Current State</Link></li>
+          </ul>
+    </nav>
+    <Route exact path="/debug" component={ConsoleCurrentState} />
+    <Route exact path="/task1" component={FormTask} />
   </div>
+  </Router>
 )
 
 export default App;
